4363e81c7c4b455b188d6b1f522f2d938d8cc4c1
Alex Gavrylov <132555815+alexgavrylov06@users.noreply.github.com>
2023-06-15 01:11:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let name = prompt('name?') || 'Johnny Sins';
-let age = prompt('age?') || 44;
-let smoking = confirm('smoking?');
+let name    =   prompt('name?') || 'Johnny Sins';
+let age     =   +prompt('age?') || 44;
+let smokingFlag =   confirm('smoking?');
 
-if (age < 18 && !smoking) {
-    console.log(`Молодець ${name}! Гарний приклад для своїх однолітків`)
-}  else if (age < 18 && smoking) {
-    console.log(`${name}, а твої батьки знають про це?`)
-}  else if (age > 18 && !smoking) {
-    console.log( `Супер ${name}! Мабуть ще й спортом займаєшся!`)
-} else if (age > 18 && smoking) {
-    console.log( `Що ж ${name}, це твій вібір. Але я не радив би курити`)
+
+let checkNameNumbers =  /\d/.test(name);
+let checkAgeLetters  =  String(age).match(/[a-z]/i);
+
+
+if (checkNameNumbers || checkAgeLetters || checkNameNumbers && checkAgeLetters) {
+    console.log('somthing wrong!!!неправильно заполнили name или age')
+} else {
+    if (age < 18 && !smokingFlag) {
+        console.log(`Молодець ${name}! Гарний приклад для своїх однолітків`)
+    }  else if (age < 18 && smokingFlag) {
+        console.log(`${name}, а твої батьки знають про це?`)
+    }  else if (age > 18 && !smokingFlag) {
+        console.log( `Супер ${name}! Мабуть ще й спортом займаєшся!`)
+    } else if (age > 18 && smokingFlag) {
+        console.log( `Що ж ${name}, це твій вібір. Але я не радив би курити`)
+    }
 }
