70ac30dc42815b2abd030e1c49a2b1dbd971318c
Alex Gavrylov <132555815+alexgavrylov06@users.noreply.github.com>
2023-06-15 13:00:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-let name    =   prompt('name?') || 'Johnny Sins';
-let age     =   +prompt('age?') || 44;
-let smokingFlag =   confirm('smoking?');
+let name             =   prompt('name?') || 'Johnny Sins';
+let age              =   +prompt('age?') || 44;
+let smokingFlag      =   confirm('smoking?');
 
 
 let checkNameNumbers =  /\d/.test(name);
 let checkAgeLetters  =  String(age).match(/[a-z]/i);
 
+function survey(checkNameNumbers, checkAgeLetters) 
 
-if (checkNameNumbers || checkAgeLetters || checkNameNumbers && checkAgeLetters) {
-    console.log('somthing wrong!!!неправильно заполнили name или age')
-} else {
-    if (age < 18 && !smokingFlag) {
-        console.log(`Молодець ${name}! Гарний приклад для своїх однолітків`)
-    }  else if (age < 18 && smokingFlag) {
-        console.log(`${name}, а твої батьки знають про це?`)
-    }  else if (age > 18 && !smokingFlag) {
-        console.log( `Супер ${name}! Мабуть ще й спортом займаєшся!`)
-    } else if (age > 18 && smokingFlag) {
-        console.log( `Що ж ${name}, це твій вібір. Але я не радив би курити`)
+{
+
+    if (checkNameNumbers || checkAgeLetters || checkNameNumbers && checkAgeLetters) {
+        return 'somthing wrong!!!неправильно заполнили name или age'
+    } else {
+        if (age < 18 && !smokingFlag) {
+            return `Молодець ${name}! Гарний приклад для своїх однолітків`
+        }  else if (age < 18 && smokingFlag) {
+            return `${name}, а твої батьки знають про це?`
+        }  else if (age > 18 && !smokingFlag) {
+            return `Супер ${name}! Мабуть ще й спортом займаєшся!`
+        } else if (age > 18 && smokingFlag) {
+            return `Що ж ${name}, це твій вібір. Але я не радив би курити`
+        }
     }
+
 }
+
+console.log( survey(checkNameNumbers, checkAgeLetters) );
+alert( survey(checkNameNumbers, checkAgeLetters) );
