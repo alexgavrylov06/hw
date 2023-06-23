64d79003c05051c01b4da714ca17d63b1b159970
Alex Gavrylov <132555815+alexgavrylov06@users.noreply.github.com>
2023-06-23 15:36:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,106 @@
-const todosList = []
-const todoForm = document.getElementById('todoForm')
-const todoList = document.getElementById('todoList')
+const todosList = [];
+const message = document.querySelector('.message')
+const todoForm = document.getElementById('todoForm');
+const todoList = document.getElementById('todoList');
 
+const field = document.getElementById('todoInput');
+
+document.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON' && e.target.id === 'btn') {
+        let spanText = e.target.previousElementSibling.textContent;
+        todosList.forEach((item, i) => {
+            if (item.text === spanText) {
+                todosList.splice(i, 1)
+            }
+            renderList()
+        })
+    }
+});
+
+document.addEventListener('click', (e) => {
+
+    let t = '', d = e.target;
+
+    if (d.tagName === 'INPUT' && d.type === 'checkbox') {
+        t = d.parentNode.id
+    }
+
+    for (i of todosList) {
+        if (+i.id === +t) {
+            i.color = !i.color
+            i.disabled = !i.disabled
+            i.status = !i.status
+        }
+        renderList()  
+    }
+
+});
 
 
 todoForm.addEventListener('submit', (e) => {
-    const fiеld =  document.getElementById('todoInput')
-    const delBtn = todoList.querySelector('button')
+    const field = document.getElementById('todoInput')
+
     e.preventDefault()
-    
+
     const newTodo = {
         id: Date.now(),
         date: new Date(),
-        text: fiеld.value,
-        completed: false,
+        text: field.value,
+        status: false,
+        color: false,
+        disabled: false,
     }
 
-    fiеld.value = '';
+    field.value = ''
 
-    todosList.push(newTodo);
+    todosList.push(newTodo)
+    
 
+    incorrectInputs()
+    
     renderList()
 });
 
-const renderList = () => {
-    let content = '';
-    todosList.map(item => {
-        content += `<li id = "${item.id}"><input type="checkbox" ${item.completed ? 'checked' : ''}><span>${item.text}</span><button disabled>Dеlatе</button></li>`
+
+
+function renderList() 
+
+{
+    
+    todoList.innerHTML = ''
+    let content = ''
+    todosList.map(todo => {
+        const ul = `<li id = '${todo.id}'><input type = 'checkbox' ${todo.status ? 'checked' : ''}> <span style = 'color:${todo.color ? 'red' : 'black'};font-size:25px;font-weight: 600;'>${todo.text}</span> <button id = 'btn' type = 'submit' ${todo.disabled ? '' : 'disabled'}>Delat</button></li>`
+        content += ul;
     })
     todoList.innerHTML = content
+    countTasks()
 }
 
-todoList.addEventListener('change', function(e) {
-
-    let textSpanInput = todoList.querySelector('span').innerHTML
 
 
-    todosList.forEach(function(item){
-        if (item.text === textSpanInput) {
-            item.completed = !item.completed;
-            console.log(item)
-        } 
+function incorrectInputs() 
+
+{
+
+    for (let i = 0; i < todosList.length; i++) 
+    {
+        if (todosList[i].text.match(/^\s*$/)) {
+            todosList.splice(i, 1)
+        }
+    }
+
+}
+  
+
+function countTasks() {
+    const compTask = todosList.filter(task => {
+        task.status === true
     })
 
-})
-
-
-
-
-
-
-
-
+    if (todosList.length === 0) {
+        message.textContent = `empty List`
+    } else {
+        message.textContent = `current amount of Tasks |${todosList.length}|`
+    }
+}
