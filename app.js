4082c669a2a799e6faf698125e009d28a298c7ea
Alex Gavrylov <132555815+alexgavrylov06@users.noreply.github.com>
2023-08-05 20:24:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.modal');
+console.log(buttons);
+
+// btn.forEach((el) =>
+//   el.addEventListener('click', () => {
+//     const target = this.dataset.target;
+//     const modal = document.querySelector(target);
+
+//     if (modal) {
+//       modal.classList.add('active');
+//     }
+//   })
+// );
